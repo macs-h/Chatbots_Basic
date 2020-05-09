10051a31d893569ca46ee87d99dea62fa72e5638
Max Huang <max@maxhuang.nz>
2020-05-09 20:22:11 +1200

    Added comments - removed one unnecessary line of code.

--- Looping_Coffee_Bot/script.py
+++ Looping_Coffee_Bot/script.py
@@ -7,12 +7,16 @@ __version__ = "1.0"
 
 from utils import get_size, get_drink_type
 
+# Main function for chatbot.
 def coffee_bot():
   order_correct = False
   more_orders = True
   drinks = []
 
+  # Starts the order process again if the order is deemed incorrect.
   while not order_correct:
+
+    # While the customer wants to place more orders, take more orders.
     while more_orders:
       size = get_size()  
       drink_type = get_drink_type()
@@ -26,23 +30,34 @@ def coffee_bot():
       if another_drink.casefold() == 'y':
         print()
         continue
+
+      # If no more orders, then break out and continue to rest of program.
       more_orders = False
     
+    # If only one drink, no need to list it out. Just a one line print.
     if len(drinks) == 1:
       print("\nOkay, so I have a {} {}".format(drinks[0][0], drinks[0][1]))
+    
+    # If multiple drink orders, then list out for easy readability.
     else: 
       print("\nOkay, so I have:")
       for drink in drinks:
         print("- A {} {}".format(drink[0], drink[1]))
       
+    # Check that the order printed out is correct.
     check_order = input("\nIs this order correct? [y/N]\n> ")
+
+    # If the order is correct, do not take another order.
+    # Move onto getting customer name.
     if check_order.casefold() == 'y':
-      order_correct = True
       break
+
+    # Order was wrong, take it again. Reset variables.
     print("\nSorry about that, let's start again.")
     more_orders = True
     drinks = []
 
+  # Get customer name - Title case the name.
   name = input('\nCan I get your name please? \n> ')
   print('\nThanks, {}! Your order will be ready shortly.'.format(name.capitalize()))
 
